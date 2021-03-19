4a50837b7e66df30aeb977c071d9c65f9de74689
Youngseob Kim <youngseob.kim@enpc.fr>
2021-03-19 15:54:18 +0100

    Added the program processing/ssh-aerosol

---
+++ processing/ssh-aerosol/munich-ssh.cpp
@@ -0,0 +1,49 @@
+
+//////////////
+// INCLUDES //
+
+#define SELDONDATA_DEBUG_LEVEL_2
+
+#include "AtmoData.hxx"
+#include "StreetDriver.cxx"
+#include "BaseOutputSaver.cxx"
+#include "StreetNetworkAerosol.cxx"
+#include "Aerosol_SSH.cxx"
+#include <time.h>
+
+using namespace Polyphemus;
+
+// INCLUDES //
+//////////////
+  
+int main(int argc, char** argv)
+{
+
+  TRY;
+  clock_t tBegin, tEnd, tPassed_time;
+  tBegin = clock();
+  
+  if (argc != 2)
+    {
+      string mesg  = "Usage:\n";
+      mesg += string("  ") + argv[0] + " [configuration file]";
+      cout << mesg << endl;
+      return 1;
+    }
+
+  typedef double real;
+  typedef StreetNetworkAerosol<real, Aerosol_SSH<real> > ClassModel;
+
+  StreetDriver<real, ClassModel, BaseOutputSaver<real, ClassModel> >
+    Driver(argv[1]);
+
+  Driver.Run();
+  tEnd = clock();
+  tPassed_time = ((tEnd - tBegin) / (CLOCKS_PER_SEC / 1000));
+  // cout<<"Passed time: "<<tPassed_time<<endl;
+
+  END;
+
+  return 0;
+
+}
