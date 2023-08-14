4aa6db67f7366ea204b1b518a9a58705d6db9de0
Youngseob Kim <youngseob.kim@enpc.fr>
2023-08-14 15:28:43 +0200

    Added a new function write_binary().

--- preprocessing/io_tools.py
+++ preprocessing/io_tools.py
@@ -17,3 +17,20 @@ def append_binary(arrayToSave, filename, type = 'f'):
     numpy.array(arrayToSave, dtype = type).tofile(f)
     f.close()
     
+def write_binary(arrayToSave, filename, type = 'f'):
+    """
+    Saves a numpy in a binary file using specified type.
+
+    @type arrayToSave: numpy.array
+    @param arrayToSave: The array to save.
+
+    @type filename: string or python file object
+    @param filename: The name of the file to save the array into.
+
+    @type type: string
+    @param type: Format of data to save the array in file.
+    """
+    f = open(filename, 'wb')
+    numpy.array(arrayToSave, dtype = type).tofile(f)
+    f.close()
+    
